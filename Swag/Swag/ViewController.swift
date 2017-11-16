8c4dd046780d58f8b36855f79464ed7fa7dba422
harish <harishchopra86@gmail.com>
2017-11-15 22:00:15 -0500

    Initial project added

---
+++ Swag/Swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swag
+//
+//  Created by Harish Chopra on 2017-11-15.
+//  Copyright © 2017 Harish Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
