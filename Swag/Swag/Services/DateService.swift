e2d90641af9a8b5d59c6809dedadc75c95024af6
Harish Kumar Chopra <harishchopra86@gmail.com>
2017-11-16 18:22:24 -0500

    Names updated

--- Swag/Swag/Services/DateService.swift
+++ Swag/Swag/Services/DateService.swift
@@ -20,23 +20,23 @@ class DataService {
     
     
     private let hats = [
-        Product(title: "Devslopes Logo Graphic Beanie", price: "$18", imageName: "hat01.png"),
-        Product(title: "Devslopes Logo Hat Black, price", price: "$22", imageName: "hat02.png"),
-        Product(title: "Devslopes Logo Hat White", price: "$26", imageName: "hat03.png"),
-        Product(title: "Devslopes Logo Snap Black", price: "$21", imageName: "hat04.png")
+        Product(title: "Graphic Beanie", price: "$18", imageName: "hat01.png"),
+        Product(title: "Hat Black, price", price: "$22", imageName: "hat02.png"),
+        Product(title: "Hat White", price: "$26", imageName: "hat03.png"),
+        Product(title: "Logo Snap Black", price: "$21", imageName: "hat04.png")
     ]
     
     private let hoodies = [
-        Product(title: "Devslopes Logo Hoodie gray", price: "$25", imageName: "hoodie01.png"),
-        Product(title: "Devslopes Logo Hoodie red", price: "$28", imageName: "hoodie02.png"),
-        Product(title: "Devslopes Logo Hoodie black", price: "$32", imageName: "hoodie03.png"),
-        Product(title: "Devslopes Logo Hoodie white", price: "$36", imageName: "hoodie04.png")
+        Product(title: "Hoodie gray", price: "$25", imageName: "hoodie01.png"),
+        Product(title: "Hoodie red", price: "$28", imageName: "hoodie02.png"),
+        Product(title: "Hoodie black", price: "$32", imageName: "hoodie03.png"),
+        Product(title: "Hoodie white", price: "$36", imageName: "hoodie04.png")
     ]
     
     private let shirts = [
-        Product(title: "Devslopes Logo Shirt black", price: "$18", imageName: "shirt01.png"),
-        Product(title: "Devslopes Badge Shirt lighy gray", price: "$28", imageName: "shirt02.png"),
-        Product(title: "Devslopes Logo Shirt Red", price: "$32", imageName: "shirt03.png"),
+        Product(title: "Shirt black", price: "$18", imageName: "shirt01.png"),
+        Product(title: "Badge Shirt lighy gray", price: "$28", imageName: "shirt02.png"),
+        Product(title: "Shirt Red", price: "$32", imageName: "shirt03.png"),
         Product(title: "Gray shirt", price: "$36", imageName: "shirt04.png"),
         Product(title: "Digital Gray shirt", price: "$40", imageName: "shirt05.png")
     ]
