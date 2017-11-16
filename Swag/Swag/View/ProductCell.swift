39813a544b9c2a4513caa1980f522a1d6a10b250
harish <harishchopra86@gmail.com>
2017-11-16 08:37:34 -0500

    new VC added for products collection view ui in new vc

---
+++ Swag/Swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Swag
+//
+//  Created by Harish Chopra on 2017-11-16.
+//  Copyright © 2017 Harish Chopra. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage:UIImageView!
+    @IBOutlet weak var productTitle:UILabel!
+    @IBOutlet weak var productPrice:UILabel!
+    
+}
