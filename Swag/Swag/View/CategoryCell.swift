4fe2007149e5042506ce1546f3f1833519a1a22c
harish <harishchopra86@gmail.com>
2017-11-15 22:18:21 -0500

    categories VC ui completed

---
+++ Swag/Swag/View/CategoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  CategoryCell.swift
+//  Swag
+//
+//  Created by Harish Chopra on 2017-11-15.
+//  Copyright © 2017 Harish Chopra. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
